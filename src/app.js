1e20a3306a74baf82a57789fd7900846c1268207
Andrew Garcia <grcwebdev@gmail.com>
2019-10-04 14:38:48 -0400

    home mobile complete

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
-import {Person} from './Person';
 import './scss/style.scss';
 
-const p = new Person('Zura');
-console.info(p.sayHello());
+// Sticky Nav
+let nav = document.getElementById('nav');
+let sticky = nav.offsetTop;
+
+window.onscroll = myFunction();
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        nav.classList.add('sticky')
+    } else {
+        nav.classList.remove('sticky')
+    }
+}
+//
+
+// Mobile Menu Pop-up
+let open = document.getElementById('menu-icon');
+let menu = document.getElementById('mobile-menu');
+console.log(open);
+open.addEventListener("click", menuIconFunction);
+
+function menuIconFunction() {
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+    } else {
+        menu.classList.add('active');
+    }
+}
