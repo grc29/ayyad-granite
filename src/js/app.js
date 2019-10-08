21b31c2623315a234c71afe1b145e3c2d4869cb0
Andrew Garcia <grcwebdev@gmail.com>
2019-10-07 20:22:23 -0400

    mobile structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 import '../scss/style.scss';
-
 // Sticky Nav
-let nav = document.getElementById('nav');
+let nav = document.getElementById('mobile-nav');
 let sticky = nav.offsetTop;
 
 window.onscroll = myFunction();
@@ -17,16 +16,21 @@ function myFunction() {
 
 // Mobile Menu Pop-up
 let open = document.getElementById('menu-icon');
-let menu = document.getElementById('mobile-menu');
+let menu = document.getElementById('mobile-dropdown');
+let container = document.getElementById('container');
 open.addEventListener("click", menuIconFunction);
 
 function menuIconFunction() {
     if (menu.classList.contains('active')) {
         menu.classList.remove('active');
+        container.classList.remove('active');
+        nav.classList.remove('active');
         open.classList.remove('fa-times');
         open.classList.add('fa-bars');
     } else {
         menu.classList.add('active');
+        container.classList.add('active');
+        nav.classList.add('active');
         open.classList.remove('fa-bars');
         open.classList.add('fa-times');
     }
